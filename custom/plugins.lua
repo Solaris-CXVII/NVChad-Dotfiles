2d48189a2126d1451061d11ee493a5899af80f2d
Solaris-CXVII <aldencua2403@gmail.com>
2024-10-16 10:10:13 +0800

    fix nvim silicon config plugin

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -187,9 +187,15 @@ local plugins = {
     "michaelrommel/nvim-silicon",
     lazy = true,
     cmd = "Silicon",
-    config = function()
-      require("silicon").setup({
-        font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+   config = function()
+      require("nvim-silicon").setup({
+        font = "JetBrainsMono Nerd Font Mono=34;Noto Color Emoji=34",
+        theme = "Dracula",
+        window_title = function()
+          return vim.fn.fnamemodify(
+            vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t"
+          )
+        end,
       })
     end
   }
